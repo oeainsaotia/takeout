16a5bdb7dfc225700e2cfb369f0ab5ce779a62ee
Administrator <13560528032@163.com>
2023-10-03 14:12:44 +0800

    订单状态定时处理

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -47,24 +47,66 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             shoppingCart.setNumber(shoppingCart.getNumber() + 1);
             shoppingCartMapper.updateNumberById(shoppingCart);
         } else {
-        //如果不存在，插入数据，数量就是1
-        Long dishId = shoppingCartDTO.getDishId();
-        if (dishId != null) {
-            //添加到购物车的是菜品
-            Dish dish = dishMapper.getById(dishId);
-            shoppingCart.setName(dish.getName());
-            shoppingCart.setImage(dish.getImage());
-            shoppingCart.setAmount(dish.getPrice());
-        } else {
-            //添加到购物车的是套餐
-            Setmeal setmeal = setmealMapper.getById(shoppingCartDTO.getSetmealId());
-            shoppingCart.setName(setmeal.getName());
-            shoppingCart.setImage(setmeal.getImage());
-            shoppingCart.setAmount(setmeal.getPrice());
+            //如果不存在，插入数据，数量就是1
+            Long dishId = shoppingCartDTO.getDishId();
+            if (dishId != null) {
+                //添加到购物车的是菜品
+                Dish dish = dishMapper.getById(dishId);
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setAmount(dish.getPrice());
+            } else {
+                //添加到购物车的是套餐
+                Setmeal setmeal = setmealMapper.getById(shoppingCartDTO.getSetmealId());
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setAmount(setmeal.getPrice());
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartMapper.insert(shoppingCart);
         }
-        shoppingCart.setNumber(1);
-        shoppingCart.setCreateTime(LocalDateTime.now());
-        shoppingCartMapper.insert(shoppingCart);
     }
-}
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    public List<ShoppingCart> showShoppingCart() {
+        return shoppingCartMapper.list(ShoppingCart.builder().userId(BaseContext.getCurrentId()).build());
+    }
+
+    /**
+     * 清空购物车
+     */
+    public void cleanShoppingCart() {
+        shoppingCartMapper.deleteByUserId(BaseContext.getCurrentId());
+    }
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     */
+    public void subShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO,shoppingCart);
+        //设置查询条件，查询当前登录用户的购物车数据
+        shoppingCart.setUserId(BaseContext.getCurrentId());
+
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+
+        if(list != null && list.size() > 0){
+            shoppingCart = list.get(0);
+
+            Integer number = shoppingCart.getNumber();
+            if(number == 1){
+                //当前商品在购物车中的份数为1，直接删除当前记录
+                shoppingCartMapper.deleteById(shoppingCart.getId());
+            }else {
+                //当前商品在购物车中的份数不为1，修改份数即可
+                shoppingCart.setNumber(shoppingCart.getNumber() - 1);
+                shoppingCartMapper.updateNumberById(shoppingCart);
+            }
+        }
+    }
 }
